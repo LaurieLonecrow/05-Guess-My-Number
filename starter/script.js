bdbd30737ac4de0a62457ae3f90b3357a98c1d1d
Laurie Lonecrow <laurieteresa@gmail.com>
2021-03-14 19:27:03 -0400

    Cleaned repetitive code

--- starter/script.js
+++ starter/script.js
@@ -8,10 +8,10 @@
 // document.querySelector('.score').textContent = 10;
 // document.querySelector('.guess').value = 23;
 // console.log(document.querySelector('.guess').value);
-const secretNumber = Math.trunc(Math.random() * 20) + 1;
+let secretNumber = Math.trunc(Math.random() * 20) + 1;
 let score = 20;
-let highscorearr = [];
-const highscore = Math.max(...highscorearr);
+let highscore = 0;
+
 
 document.querySelector('.check').addEventListener('click', function() {
   const guess = Number(document.querySelector('.guess').value);
@@ -23,24 +23,15 @@ document.querySelector('.check').addEventListener('click', function() {
       "Correct Number!"
     document.querySelector('body').style.backgroundColor = '#60b347';
     document.querySelector('.number').style.width = '30rem';
-    document.querySelector('.number').textContent = secretNumber;
-    highscorearr.push(score);
-
-  } else if (guess > secretNumber) {
-    if (score > 1) {
-      document.querySelector('.message').textContent =
-        "Too high!";
-      score--;
-      document.querySelector('.score').textContent =
-        score;
-    } else {
-      document.querySelector('.message').textContent =
-        "You lost the game!";
+    if (score > highscore) {
+      highscore = score;
+      document.querySelector('.highscore').textContent = highscore;
     }
-  } else if (guess < secretNumber) {
+
+  } else if (guess !== secretNumber) {
     if (score > 1) {
       document.querySelector('.message').textContent =
-        "Too low!";
+        guess > secretNumber ? "Too high!" : "Too low!";
       score--;
       document.querySelector('.score').textContent =
         score;
@@ -51,18 +42,16 @@ document.querySelector('.check').addEventListener('click', function() {
   }
 });
 
-console.log(highscorearr);
-console.log(highscore);
 
 document.querySelector('.again').addEventListener('click', function() {
-  document.querySelector('.score').textContent = 20;
+  score = 20;
+  secretNumber = Math.trunc(Math.random() * 20) + 1;
+  document.querySelector('.score').textContent = score;
   document.querySelector('.message').textContent = "Start guessing...";
   document.querySelector('body').style.backgroundColor = '#222';
   document.querySelector('.number').style.width = '15rem';
-  document.querySelector('.number').value = '?';
+  document.querySelector('.number').textContent = '?';
   document.querySelector('.guess').value = "";
-  document.querySelector('.highscore').textContent = "";
+  document.querySelector('.number').value = secretNumber;
+
 });
-
-
-//highscore is the highest Score, taken from the score, store scores and loop through to find hte max.
